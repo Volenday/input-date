d8e4a4874cd09bafd289d54d8d63634d62fa67fe
Gabriel Joshua Paet <gabriel.paet@volenday.com>
2021-07-01 09:27:18 +0800

    v2.2.2

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ export default ({
 	error = null,
 	extra = null,
 	id,
+	inlineError = true,
 	label = '',
 	onChange,
 	onOk = null,
@@ -52,9 +53,8 @@ export default ({
 		);
 	};
 
-	const formItemCommonProps = {
+	let formItemCommonProps = {
 		colon: false,
-		help: error ? error : '',
 		label: withLabel ? (
 			<>
 				<div style={{ float: 'right' }}>{extra}</div> <span class="label">{label}</span>
@@ -65,6 +65,7 @@ export default ({
 		required,
 		validateStatus: error ? 'error' : 'success'
 	};
+	if (inlineError) formItemCommonProps = { ...formItemCommonProps, help: error ? error : '' };
 
 	return (
 		<Form.Item {...formItemCommonProps}>
